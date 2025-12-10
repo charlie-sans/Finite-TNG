cc0e43ac494abe6b139e1d07ca61af1593c9b8b9
charlie-sans <gamerboy030607@gmail.com>
2025-12-10 13:30:16 +1100

    Refactor code structure for improved readability and maintainability

--- src/components/projectui.jsx
+++ src/components/projectui.jsx
@@ -3,7 +3,7 @@ import InfiniteMenu from './InfiniteMenu'
 
 const items = [
   {
-    image: '',
+    image: '/images/objectir.png',
     link: 'https://google.com/',
     title: 'Item 1',
     description: 'This is pretty cool, right?',
@@ -14,11 +14,11 @@ const items = [
     hoverStyles: { bgColor: '#3b82f6', textColor: '#ffffff' }
   },
   {
-    image: '',
+    image: '/images/purr.png',
     link: 'https://google.com/',
     title: 'Item 1',
     description: 'This is pretty cool, right?',
-        label: 'home',
+    label: 'home',
     href: '#',
     ariaLabel: 'Home',
     rotation: -8,
@@ -35,8 +35,8 @@ const items = [
     rotation: -8,
     hoverStyles: { bgColor: '#3b82f6', textColor: '#ffffff' }
   }
-  
 ];
+
 export const Projects = () => {
   return (
     <div style={{ minHeight: '100vh', width: '100vw', margin: 0, padding: 0, position: 'relative', overflow: 'hidden' }}>
@@ -46,5 +46,4 @@ export const Projects = () => {
       </div>
     </div>
   );
-
 }
